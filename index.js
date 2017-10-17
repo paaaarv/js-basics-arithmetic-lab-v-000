b8b1f471c4ff4bf949a85e1a869e547b631d2cfe
Parv Kumar <parvathikumar92@gmail.com>
2017-10-17 02:09:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ let newID = oldID + 1000000000
 const ageIsValid = Number.isInteger(currentAge)
 
 const randomNumber = Math.floor(Math.random() * 10) + 10
+
+const randomInteger = randomNumber.parseInt
